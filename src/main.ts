765c494afebfd27c2c32f7e4313370362e967218
Sonja-Kitanoska <sonja.kitanoska@appliedtechnology.se>
2023-02-21 15:38:54 +0100

    removed containter again

--- src/main.ts
+++ src/main.ts
@@ -15,13 +15,12 @@ const fetchData = async () => {
 
   const photos = await fetch(`https://api.unsplash.com/search/photos?page=${count}&query=${search}&client_id=${import.meta.env.VITE_ACCESS_KEY}`);
   const data = await photos.json();
-  console.log(data);
   data.results.forEach((photo: Photo) => {
     document.querySelector('#main-container__images')!.innerHTML += `
     <div class="flip-card">
       <div class="flip-card-inner">
         <div class="flip-card-front">
-          <img class="main-containter__photo" src="${photo.urls.small}" alt="${photo.alt_description}">
+          <img class="main-container__photo" src="${photo.urls.small}" alt="${photo.alt_description}">
         </div>
         <div class="flip-card-back">
           <h1>Unsplash link</h1>
@@ -29,23 +28,23 @@ const fetchData = async () => {
           <p>photographer profile link</p>
         </div>
       </div>
-     </div>`
+     </div>`;
   });
 };
 const renderSearchResults = (localStorageData: string[]) => {
   document.querySelector('#ul-element')!.innerHTML = '';
-  localStorageData.slice(0, 3).map((searchTerm: string) => {
-    document.querySelector('#ul-element')!.innerHTML+=`
+  localStorageData.slice(0, 3).forEach((searchTerm: string) => {
+    document.querySelector('#ul-element')!.innerHTML += `
       <li>${searchTerm}</li>
-    `
-  })
+    `;
+  });
   if (state.length !== 0) {
     document.querySelector('#input-container__outer')!.classList.add('input-container__outer');
   }
-}
-document.querySelector("#search-bar")?.addEventListener('click', () => {
-  renderSearchResults(state)
-} );
+};
+document.querySelector('#search-bar')?.addEventListener('click', () => {
+  renderSearchResults(state);
+});
 
 document.querySelector('#search-btn')?.addEventListener('click', () => {
   count = 1;
@@ -54,24 +53,18 @@ document.querySelector('#search-btn')?.addEventListener('click', () => {
 });
 
 document.querySelector('#prev-btn')?.addEventListener('click', () => {
-  if(count > 1) {
-    count--
+  if (count > 1) {
+    count -= 1;
     fetchData();
   }
-  if(count === 1) {
-    document.querySelector('#prev-btn')?.classList.add('hidden')
+  if (count === 1) {
+    document.querySelector('#prev-btn')?.classList.add('hidden');
   }
-})
+});
 document.querySelector('#next-btn')?.addEventListener('click', () => {
-    count++
-    fetchData();
-    document.querySelector('#prev-btn')?.classList.remove('hidden')
-})
-// document.querySelector('.main-container')?.addEventListener('click', () => {
-//   document.querySelector('#input-container__outer')!.classList.remove('input-container__outer');
-// })
-
-
-
+  count += 1;
+  fetchData();
+  document.querySelector('#prev-btn')?.classList.remove('hidden');
+});
 
 export { add };
