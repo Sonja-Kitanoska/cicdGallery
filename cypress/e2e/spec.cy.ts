97ae00a05a2ad8ff5e60d08c29f65e706264919b
Sonja-Kitanoska <sonja.kitanoska@appliedtechnology.se>
2023-02-22 10:59:07 +0100

    removed secret test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -7,8 +7,8 @@ describe('template spec', () => {
   describe('checking HTML', () => {
     it('should have 10 images', () => {
       cy.get('.main-container__searchbar').type('cats');
-      cy.get('#search-btn').click();
-      cy.get('main').find('img').should('have.length', 10);
+      // cy.get('#search-btn').click();
+      // cy.get('main').find('img').should('have.length', 10);
     });
   });
 });
