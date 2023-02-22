4044d536ea8ed034a8dae4abbc8da7657a8e64b9
Sonja-Kitanoska <sonja.kitanoska@appliedtechnology.se>
2023-02-22 11:41:10 +0100

    google test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,15 +2,16 @@
 
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://127.0.0.1:5173/');
+    // cy.visit('http://127.0.0.1:5173/');
+    cy.visit('https://google.com');
   });
-  describe('checking HTML', () => {
-    it('should have 10 images', () => {
-      cy.get('.main-container__searchbar').type('cats');
-      // cy.get('#search-btn').click();
-      // cy.get('main').find('img').should('have.length', 10);
-    });
-  });
+  // describe('checking HTML', () => {
+  //   it('should have 10 images', () => {
+  //     cy.get('.main-container__searchbar').type('cats');
+  //     // cy.get('#search-btn').click();
+  //     // cy.get('main').find('img').should('have.length', 10);
+  //   });
+  // });
 });
 
 export {};
